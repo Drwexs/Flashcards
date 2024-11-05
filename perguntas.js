4a76abe8fba4476900bae6ff4accfb5be5a524d9
Drwexs <146848206+Drwexs@users.noreply.github.com>
2024-11-05 08:49:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Brawl Stars',
+    'Quais foram os brawlers mais feio da história do brawl?',
+    'Shelly e Dynamike'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'Qual é a temporada 21?',
+    'Circo Bizarro'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'Qual dos brawlers quase foi removido completamente por quebrar o jogo e travar/lagar todos os servidores?',
+    'Eugênio'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'Qual modo de jogo que não está desde o início do jogo?',
+    'Encurralado'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'Mais você sabe em que ano foi fundado brawl stars?',
+    '2017'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'Qual primeiro brawlers foi adicionado ao jogo logo depois que brawl stars foi lançado? (ou seja, o primeiro brawler a ser lançado sem ser original do beta)',
+    'Piper'
+)
